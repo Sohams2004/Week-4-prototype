e2b766702150164dff29a1719590b89fcd84f9ef
Soham Shinde <sohamsshinde04@gmail.com>
2024-03-06 12:58:41 +0400

    Keypad
    Made a functional keypad for keypad puzzle.

--- Assets/Soham Stuff/Scripts/ObjectPickUp.cs
+++ Assets/Soham Stuff/Scripts/ObjectPickUp.cs
@@ -1,27 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class ObjectPickUp : MonoBehaviour
 {
     [SerializeField] float rayLength;
 
+    [SerializeField] bool isPicked;
+
     [SerializeField] LayerMask object_;
 
     [SerializeField] Transform pickUpPoint;
 
+    [SerializeField] Rigidbody objectRb;
+
+    [SerializeField] GameObject pickableObject, player;
+
+    RaycastHit hit;
+
+    private void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
+
     void ObjectDetect()
     {
-        if (Physics.Raycast(transform.position, transform.forward, out RaycastHit hit, rayLength, object_))
+        if (Physics.Raycast(transform.position, transform.forward, out hit, rayLength, object_))
         {
             Debug.Log("Object detected");
             GameObject hitObj = hit.collider.gameObject;
 
-            if(Input.GetMouseButton(0))
+            if (Input.GetMouseButtonDown(0))
             {
+                objectRb = hitObj.GetComponent<Rigidbody>();
+                pickableObject = hitObj.gameObject;
                 hitObj.transform.position = pickUpPoint.position;
+                objectRb.constraints = RigidbodyConstraints.FreezePositionY;
             }
         }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            objectRb.constraints = RigidbodyConstraints.None;
+        }
+    }
+
+    void ObjectPick()
+    {
+
     }
 
     private void OnDrawGizmos()
