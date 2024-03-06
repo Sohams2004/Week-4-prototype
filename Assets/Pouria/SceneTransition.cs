ad9718a61b1054d23ba94f02c9efe0ce12f90fd4
Pouria <p.dehghani@student.saedubai.com>
2024-03-06 15:21:52 +0400

    Added a public door to use , will explain how to use on whatsapp

---
+++ Assets/Pouria/SceneTransition.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransition : MonoBehaviour
+{
+    //Set the name in the inspector pls ty
+    public string targetScene;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        SceneManager.LoadScene(targetScene);
+    }
+}
