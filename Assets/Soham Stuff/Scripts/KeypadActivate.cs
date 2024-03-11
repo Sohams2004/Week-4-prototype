4416dddd4b4d559480a56b7fd7677504f719cbd9
Adil zain <adilzain009@gmail.com>
2024-03-11 14:18:57 +0400

    FFinal push
    Made a victory screen.

--- Assets/Soham Stuff/Scripts/KeypadActivate.cs
+++ Assets/Soham Stuff/Scripts/KeypadActivate.cs
@@ -2,8 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 //using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class KeypadActivate : Keypad
+public class KeypadActivate : Keypad 
 {
     [SerializeField] Animator vaultDoor;
 
@@ -17,7 +18,17 @@ public class KeypadActivate : Keypad
         if(isUnlocked == true)
         {
             vaultDoor.enabled = true;
+            StartCoroutine(Winscreen());
         }
+    }
+
+    IEnumerator Winscreen()
+    {
+        yield return new WaitForSeconds(2);
+        Scene scene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(scene.buildIndex + 1);
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
     }
 
     private void Update()
